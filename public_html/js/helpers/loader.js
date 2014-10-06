eb0f2b4b1fd5c6a239abbf5c2d8bb2e9852e02fc
marlon-rodrigues <marlon.gerodrigues@gmail.com>
2014-10-05 21:48:39 -0500

    Header Work

---
+++ public_html/js/helpers/loader.js
@@ -0,0 +1,9 @@
+/*
+ * Loader class for header and foot
+ */
+$(function(){
+    //every page will have these 2 elements
+    $("#header_nav").load("helpers/header.html"); 
+    $("#footer_nav").load("helpers/footer.html");
+});
+
